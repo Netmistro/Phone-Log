9e7b63a9f55d3620219126900c7291aebec5995b
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2020-03-16 09:41:38 -0400

    Added New Forms

---
+++ Phone Log/frmCalls.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmCalls
+
+End Class
---
+++ Phone Log/frmLogin.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPrice = New System.Windows.Forms.Button()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.lblUserName = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnPrice
+        '
+        Me.btnPrice.BackColor = System.Drawing.Color.DarkOrange
+        Me.btnPrice.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrice.Location = New System.Drawing.Point(179, 148)
+        Me.btnPrice.Name = "btnPrice"
+        Me.btnPrice.Size = New System.Drawing.Size(90, 35)
+        Me.btnPrice.TabIndex = 3
+        Me.btnPrice.Text = "&Login"
+        Me.btnPrice.UseVisualStyleBackColor = False
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.BackColor = System.Drawing.Color.DarkOrange
+        Me.btnBrowse.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBrowse.Location = New System.Drawing.Point(277, 148)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(90, 35)
+        Me.btnBrowse.TabIndex = 4
+        Me.btnBrowse.Text = "&Cancel"
+        Me.btnBrowse.UseVisualStyleBackColor = False
+        '
+        'lblUserName
+        '
+        Me.lblUserName.AutoSize = True
+        Me.lblUserName.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUserName.Location = New System.Drawing.Point(32, 66)
+        Me.lblUserName.Name = "lblUserName"
+        Me.lblUserName.Size = New System.Drawing.Size(67, 16)
+        Me.lblUserName.TabIndex = 1
+        Me.lblUserName.Text = "Username"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUsername.Location = New System.Drawing.Point(102, 59)
+        Me.txtUsername.Multiline = True
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(265, 30)
+        Me.txtUsername.TabIndex = 1
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(102, 112)
+        Me.txtPassword.Multiline = True
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(265, 30)
+        Me.txtPassword.TabIndex = 2
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPassword.Location = New System.Drawing.Point(34, 119)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(65, 16)
+        Me.lblPassword.TabIndex = 4
+        Me.lblPassword.Text = "Password"
+        '
+        'lblHeader
+        '
+        Me.lblHeader.AutoSize = True
+        Me.lblHeader.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.ForeColor = System.Drawing.Color.LightCoral
+        Me.lblHeader.Location = New System.Drawing.Point(35, 9)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(316, 22)
+        Me.lblHeader.TabIndex = 5
+        Me.lblHeader.Text = "Welcome to ""Phone-Log System"""
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(383, 209)
+        Me.Controls.Add(Me.lblHeader)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.btnPrice)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.lblUserName)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "frmLogin"
+        Me.Text = "Phone-Log System Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPrice As Button
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents lblUserName As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents lblHeader As Label
+End Class
